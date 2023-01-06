a898d3d03b542e6fd44523234dd91ef05b8cf75b
zhaozhuobin <zbzhao2@iflytek.com>
2023-01-06 14:42:25 +0800

    fix PLUGIN_HOME 的赋值逻辑

--- src/utils/lisa.ts
+++ src/utils/lisa.ts
@@ -2,8 +2,7 @@ import { pathExists, readJson } from 'fs-extra';
 import { homedir } from 'os';
 import { join } from 'path';
 
-// TODO: 适配 LISA_HOME
-const PLUGIN_HOME = join(homedir(), '.listenai', 'lisa-zephyr');
+const PLUGIN_HOME = join(process.env.LISA_HOME ?? join(homedir(), '.listenai'), 'lisa-zephyr');
 
 export function getLisaWest(): string {
   const binaryDir = join(PLUGIN_HOME, 'venv', process.platform === 'win32' ? 'Scripts' : 'bin');
